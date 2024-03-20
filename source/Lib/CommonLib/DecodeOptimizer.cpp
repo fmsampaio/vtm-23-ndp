7e5a9426124a05bbc48208ae2277b1d914595f1d
Felipe Sampaio <felipe.sampaio@farroupilha.ifrs.edu.br>
2024-03-20 13:41:17 -0300

    Adding mvs data at mvsDataMap

--- source/Lib/CommonLib/DecodeOptimizer.cpp
+++ source/Lib/CommonLib/DecodeOptimizer.cpp
@@ -1,6 +1,7 @@
 #include "DecodeOptimizer.h"
 
 FILE* DecodeOptimizer::mvsFile;
+std::map<std::string, MvLogData*> DecodeOptimizer::mvsDataMap;
 
 void DecodeOptimizer::openMvsFile(std::string fileName) {
     mvsFile = fopen(fileName.c_str(), "r");
@@ -21,7 +22,25 @@ void DecodeOptimizer::openMvsFile(std::string fileName) {
         if(res == 0) {
             break;
         }
-        printf("[M] %d %d %d %d %d %d %d %d %d %d\n", currFramePoc, xPU, yPU, wPU, hPU, refList, refFramePoc, xMV, yMV, fracPosition);
+        MvLogData* mvData = new MvLogData();
+        
+        mvData->currFramePoc = currFramePoc;
+        mvData->xPU = xPU;
+        mvData->yPU = yPU;
+        mvData->wPU = wPU;
+        mvData->hPU = hPU;
+        mvData->refList = refList;
+        mvData->refFramePoc = refFramePoc;
+        mvData->xMV = xMV;
+        mvData->yMV = yMV;
+        mvData->fracPosition = fracPosition;
+
+        std::string key = std::to_string(currFramePoc) + "_" +
+                          std::to_string(xPU) + "_" +
+                          std::to_string(yPU) + "_" +
+                          std::to_string(refList);
+        
+        mvsDataMap.insert({key, mvData});
     }
    
 }
--- source/Lib/CommonLib/DecodeOptimizer.h
+++ source/Lib/CommonLib/DecodeOptimizer.h
@@ -1,12 +1,29 @@
 #include <cstdio>
 #include <string>
+#include <map>
+#include <iostream>
 
 using PosType  = int32_t;
 using SizeType = uint32_t;
 
+class MvLogData {
+    public:
+        int currFramePoc;
+        PosType xPU;
+        PosType yPU;
+        SizeType wPU;
+        SizeType hPU;
+        int refList;
+        int refFramePoc;
+        int xMV;
+        int yMV;
+        int fracPosition;
+};
+
 class DecodeOptimizer {
     private:
         static FILE* mvsFile;
+        static std::map<std::string, MvLogData*> mvsDataMap;
 
     public:
         static void openMvsFile(std::string fileName);
